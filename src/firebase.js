bdcb2b7511ad200479aa2c4678c02e2c22bc783c
Doug Revell <doug.revelljr@gmail.com>
2024-10-28 11:28:22 -0400

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,21 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { getStorage } from 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhhKyQ3rOlDojQlgreXaiQ0W-TnblBMWo",
-  authDomain: "fuller-chiro-d66b1.firebaseapp.com",
-  projectId: "fuller-chiro-d66b1",
-  storageBucket: "fuller-chiro-d66b1.appspot.com",
-  messagingSenderId: "899728461916",
-  appId: "1:899728461916:web:8e6d4dcc0cf40c01ff5699",
-  measurementId: "G-4FWTGHZP49"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const storage = getStorage(app);
-
-// Initialize Firestore
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export { db, storage };
